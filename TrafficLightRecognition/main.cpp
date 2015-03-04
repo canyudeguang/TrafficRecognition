16f442c48b3a5b6d09c4c6361dd5c98d2e29e293
[kimheetae] <[kimheetae90@gmail.com]>
2015-03-04 20:54:49 +0900

    edit value & design backGroundCheck (left regionGrowing & backgroundCheck)

--- TrafficLightRecognition/main.cpp
+++ TrafficLightRecognition/main.cpp
@@ -3,16 +3,14 @@
 
 using namespace cv;
 
-#define THRESHOLD_UNDER 125
+#define THRESHOLD_UNDER 150
 #define THRESHOLD_UPPER 256
 #define MORPH_SIZE 3	//Top-Hat Kernel Size
 #define CLOSING_SIZE 1	//Closing Size
 #define DISTANCE_WITH_LIGHT 80
-#define DISTANCE_WITH_BACKGROUND 200
+#define DISTANCE_WITH_BACKGROUND 110
 
-
-///////////////////////////////////////////////////////////////////////////////////////////
-
+/*///////////////////////////////////////////////////////////////////////////////////////////*/
 /* Morphology Setting */
 
 int morph_size = MORPH_SIZE;
@@ -20,13 +18,11 @@ Mat element_tophat = getStructuringElement(MORPH_RECT, Size(2 * morph_size + 1, 
 
 int closing_size = CLOSING_SIZE;
 Mat element_close = getStructuringElement(MORPH_RECT, Size(2 * closing_size + 1, 2 * closing_size + 1), Point(closing_size, closing_size));
-
-///////////////////////////////////////////////////////////////////////////////////////////
-
+/*///////////////////////////////////////////////////////////////////////////////////////////*/
 
 
-///////////////////////////////////////////////////////////////////////////////////////////
 
+/*///////////////////////////////////////////////////////////////////////////////////////////*/
 /* Labeling Setting */
 
 int *label;	//Labeling
@@ -37,18 +33,20 @@ int objectN = 0;
 struct Object	//Labeled Object
 {
 public:
+	int count;
+	int centerX;
+	int centerY;
 	int width;
 	int height;
 	bool checking;
 	bool isDeleted;
-	Object() : width(0), height(0), checking(false), isDeleted(false) {};
+	Object() : count(0), centerX(0), centerY(0), width(0), height(0), checking(false), isDeleted(false) {};
 };
 
 //Labeling Direction
 const int dx[] = { +1, 0, -1, 0 };
 const int dy[] = { 0, +1, 0, -1 };
-
-///////////////////////////////////////////////////////////////////////////////////////////
+/*///////////////////////////////////////////////////////////////////////////////////////////*/
 
 
 /* recursive로 구현된 Labeing함수 */
@@ -81,10 +79,13 @@ void checkArea(Object object[], Mat& src)
 {
 	for (int i = labelWidth / 10-1; i < labelWidth / 10 * 9+1; i++)
 	{
-		for (int j = 0; j < labelHeight / 3 * 2; j++)
+		for (int j = 0; j < labelHeight / 3 * 2+1; j++)
 		{
 			if (label[j*labelWidth + i] > 0)
 			{
+				object[label[j*labelWidth + i]-1].count++;
+				object[label[j*labelWidth + i]-1].centerX += i;
+				object[label[j*labelWidth + i]-1].centerY += j;
 				if (object[label[j*labelWidth + i]-1].checking)
 					continue;
 				else
@@ -121,6 +122,9 @@ void checkArea(Object object[], Mat& src)
 
 	for (int k = 0; k < objectN; k++)
 	{
+		object[k].centerX /= object[k].count;
+		object[k].centerY /= object[k].count;
+
 		if (object[k].width > 10)
 		{
 			if (object[k].width * 10 / 7 < object[k].height || object[k].height * 10 / 7 < object[k].width)
@@ -157,6 +161,7 @@ void checkArea(Object object[], Mat& src)
 		}
 	}
 }
+
 
 /* Lebeing 시작하는 함수 */
 int find_components(Mat& sourceImage) {
@@ -205,30 +210,92 @@ void makeWhite(Mat& dst, Mat& src,Mat& tophatImage)
 			int green = src.at<Vec3b>(i, j)[1];
 			int red = src.at<Vec3b>(i, j)[2];
 
+			double distanceYello = calcDistance(blue, green, red, 100, 220, 255);
+			double distanceGreen = calcDistance(blue, green, red, 150, 200, 70);
+			double distanceRed = calcDistance(blue, green, red, 110, 110, 245);
+
 			if (tophatImage.at<uchar>(i,j) > 30)
 			{
-				double distanceYello = calcDistance(blue, green, red, 100, 220, 255);
-				double distanceGreen = calcDistance(blue, green, red, 150, 200, 70);
-				double distanceRed = calcDistance(blue, green, red, 110, 110, 245);
 
 				if (distanceYello < DISTANCE_WITH_LIGHT || distanceGreen < DISTANCE_WITH_LIGHT || distanceRed < DISTANCE_WITH_LIGHT)
 				{
 					dst.at<uchar>(i, j) = 255;
 				}
-				else if (distanceYello < DISTANCE_WITH_LIGHT + 40 || distanceGreen < DISTANCE_WITH_LIGHT + 40 || distanceRed < DISTANCE_WITH_LIGHT + 40)
+				else if (distanceYello < DISTANCE_WITH_LIGHT + 20 || distanceGreen < DISTANCE_WITH_LIGHT + 20 || distanceRed < DISTANCE_WITH_LIGHT + 20)
 				{
-					dst.at<uchar>(i, j) = 70;
+					dst.at<uchar>(i, j) += 50;
 				}
-				else if (distanceYello > DISTANCE_WITH_BACKGROUND && distanceGreen > DISTANCE_WITH_BACKGROUND && distanceRed > DISTANCE_WITH_BACKGROUND)
+
+				if (distanceYello > DISTANCE_WITH_BACKGROUND && distanceGreen > DISTANCE_WITH_BACKGROUND && distanceRed > DISTANCE_WITH_BACKGROUND)
 				{
 					dst.at<uchar>(i, j) = 0;
 				}
 			}
-			else
+		}
+	}
+}
+
+void regionGrowing(Object object[], Mat& dst, Mat& src)
+{
+	
+}
+
+void checkSurround(Object object[], Mat& src)
+{
+	for (int k = 0; k < objectN; k++)
+	{
+		if (!object[k].isDeleted)
+		{
+			int blackNum = 0;
+			int areaWindow = 0;
+			int multipleNum = 3;
+			if (object[k].count < 20) multipleNum = 5;
+			int left = object[k].centerX - multipleNum * 3 * object[k].width;
+			int right = object[k].centerX + multipleNum * object[k].width / 2;
+			int up = object[k].centerY - multipleNum * object[k].height;
+			int down = object[k].centerY + multipleNum * object[k].height;
+
+			if (left < 0 || right>labelWidth || up < 0 || down >labelHeight) continue;
+
+			areaWindow = (right - left)*(down - up);
+
+			for (int i = left; i < right; i++)
 			{
-				dst.at<uchar>(i, j) = 0;
+				for (int j = up; j < down; j++)
+				{
+					int blue = src.at<Vec3b>(j, i)[0];
+					int green = src.at<Vec3b>(j, i)[1];
+					int red = src.at<Vec3b>(j, i)[2];
+					if (calcDistance(blue, green, red, 20, 20, 20) < 40) blackNum++;
+				}
 			}
 
+			if (blackNum < areaWindow*0.5) object[k].isDeleted = true;
+			else continue;
+
+			blackNum = 0;
+			areaWindow = 0;
+			left = object[k].centerX - multipleNum * object[k].width / 2;
+			right = object[k].centerX + multipleNum * 3 * object[k].width;
+			up = object[k].centerY - multipleNum * object[k].height;
+			down = object[k].centerY + multipleNum * object[k].height;
+
+			if (left < 0 || right>labelWidth || up < 0 || down >labelHeight) continue;
+
+			areaWindow = (right - left)*(down - up);
+
+			for (int i = left; i < right; i++)
+			{
+				for (int j = up; j < down; j++)
+				{
+					int blue = src.at<Vec3b>(j, i)[0];
+					int green = src.at<Vec3b>(j, i)[1];
+					int red = src.at<Vec3b>(j, i)[2];
+					if (calcDistance(blue, green, red, 30, 30, 30) < 40) blackNum++;
+				}
+			}
+
+			if (blackNum < areaWindow*0.5) object[k].isDeleted = true;
 		}
 	}
 }
@@ -236,7 +303,7 @@ void makeWhite(Mat& dst, Mat& src,Mat& tophatImage)
 void main(void)
 {
 	/* Setting */
-	Mat image = imread("test4.jpg");
+	Mat image = imread("test1.jpg");
 	Mat grayScaleImage(image.rows, image.cols, CV_8UC1);
 	Mat topHatImage(image.rows, image.cols, CV_8UC1);
 	Mat thresholdedImage(image.rows, image.cols, CV_8UC1);
@@ -251,21 +318,24 @@ void main(void)
 
 	/* White Top Hat */
 	morphologyEx(grayScaleImage, topHatImage, CV_MOP_TOPHAT, element_tophat);
+
+	/* Make Spot Bright and extra color black */
 	makeWhite(topHatImage, image, topHatImage);
 
 	/* Thresholding */
 	threshold(topHatImage, thresholdedImage, THRESHOLD_UNDER, THRESHOLD_UPPER, CV_THRESH_BINARY);
-
+	
 	/* First Filter(Area) */
 	objectN = find_components(thresholdedImage);
 	Object* object = new Object[objectN];
 	checkArea(object, thresholdedImage);
-	
-	/* Second Filter */
-	// 1. Region Growing Image 생성
-	// 2. filteredImage와 비교하여 큰 차이가 보이는 경우 제거
 
-	
+	/* Second Filter(Region Growing */
+	regionGrowing(object,thresholdedImage,grayScaleImage);
+
+	/* Final Filter(Is Background Black?) */
+	checkSurround(object, image);
+
 
 	imshow("image", image);
 	imshow("grayScaleImage", grayScaleImage);
