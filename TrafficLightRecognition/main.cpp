3260e1d2f00dc77e159eae9dfa1fed74b06492ad
[kimheetae] <[kimheetae90@gmail.com]>
2015-03-13 01:30:23 +0900

    reject candidate & find blackObject

--- TrafficLightRecognition/main.cpp
+++ TrafficLightRecognition/main.cpp
@@ -3,30 +3,22 @@
 
 using namespace cv;
 
-#define THRESHOLD_UNDER 100
-#define THRESHOLD_UPPER 256
-#define MORPH_SIZE 3	//Top-Hat Kernel Size
+#define THRESHOLD_LIGHT 100
+#define THRESHOLD_BOX 70
+#define TOP_HAT_SIZE 3
 #define DISTANCE_WITH_LIGHT 60
+#define DISTANCE_REJECT_CANDIDATE_WITH_COLOR 90
 
-/*///////////////////////////////////////////////////////////////////////////////////////////*/
-					/* Morphology Setting */
 
-int morph_size = MORPH_SIZE;
+int morph_size = TOP_HAT_SIZE;
 Mat element_tophat = getStructuringElement(MORPH_RECT, Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size));
 
-/*///////////////////////////////////////////////////////////////////////////////////////////*/
-
-
-
-/*///////////////////////////////////////////////////////////////////////////////////////////*/
-					/* Labeling Setting */
-
-int *label;
 int labelWidth;
 int labelHeight;
 int objectN = 0;
+int blackObjectN = 0;
 
-struct Object	//Labeled Object
+struct Object
 {
 public:
 	int count;
@@ -42,7 +34,6 @@ public:
 //Labeling Direction
 const int dx[] = { +1, 0, -1, 0 };
 const int dy[] = { 0, +1, 0, -1 };
-/*///////////////////////////////////////////////////////////////////////////////////////////*/
 
 
 
@@ -53,7 +44,7 @@ double calcDistance(int b1, int g1, int r1, int b2, int g2, int r2)
 }
 
 /* recursive로 구현된 Labeing함수 */
-void labeling(int x, int y,int current_label, Mat& sourceImage) {
+void labeling(int x, int y,int current_label, Mat& sourceImage, int label[]) {
 	if (x < 0 || x > sourceImage.cols - 1)
 	{
 		return;
@@ -73,12 +64,12 @@ void labeling(int x, int y,int current_label, Mat& sourceImage) {
 	// recursively mark the neighbors
 	for (int direction = 0; direction < 4; ++direction)
 	{
-		labeling(x + dx[direction], y + dy[direction], current_label, sourceImage);
+		labeling(x + dx[direction], y + dy[direction], current_label, sourceImage,label);
 	}
 }
 
 /* Object를 Reject하기위해 넓이와 높이 계산하는 함수 */
-void checkArea(Object object[], Mat& src, Mat& original)
+void checkArea(Object object[], Mat& src, Mat& original, int label[])
 {
 	for (int i = labelWidth / 10-1; i < labelWidth / 10 * 9+1; i++)
 	{
@@ -140,7 +131,7 @@ void checkArea(Object object[], Mat& src, Mat& original)
 }
 
 /* Lebeing 시작하는 함수 */
-int find_components(Mat& sourceImage) {
+int find_components(Mat& sourceImage, int label[]) {
 	int component = 0;
 
 	for (int i = 0; i < sourceImage.cols; ++i)
@@ -149,7 +140,7 @@ int find_components(Mat& sourceImage) {
 		{
 			if (label[j*sourceImage.cols + i] == 0 && sourceImage.at<uchar>(j, i) > 0)
 			{
-				labeling(i, j, ++component, sourceImage);
+				labeling(i, j, ++component, sourceImage, label);
 			}
 		}
 	}
@@ -169,7 +160,7 @@ void makeWhite(Mat& dst, Mat& src,Mat& tophatImage)
 				dst.at<uchar>(i, j) = 0;
 				continue;
 			}
-			
+
 			if (j<dst.cols / 10 || j>dst.cols / 10 * 9)
 			{
 				dst.at<uchar>(i, j) = 0;
@@ -204,95 +195,103 @@ void makeWhite(Mat& dst, Mat& src,Mat& tophatImage)
 	}
 }
 
-//
-//void regionGrowing()
-//{
-//
-//}
-//
-//void checkRegion(Object object[],Mat& grayScaleImage)
-//{
-//
-//	for (int k = 0; k < objectN; k++)
-//	{
-//		Mat temp(labelHeight, labelWidth, CV_8UC1, Scalar(0));
-//		regionGrowing();
-//		
-//	}
-//}
-
-
-void checkSurround(Object object[], Mat& src)
+/* 후보들에서 색깔을 읽어서 빨,주,노와 거리가 멀면 거르는 함수 */
+void removeDifferColorCandidate(Mat& image, Object object[])
 {
 	for (int k = 0; k < objectN; k++)
 	{
-		if (!object[k].isDeleted)
+		int blue = image.at<Vec3b>(object[k].centerY, object[k].centerX)[0];
+		int green = image.at<Vec3b>(object[k].centerY, object[k].centerX)[1];
+		int red = image.at<Vec3b>(object[k].centerY, object[k].centerX)[2];
+
+		double distanceYello = calcDistance(blue, green, red, 100, 220, 255);
+		double distanceGreen = calcDistance(blue, green, red, 143, 250, 77);
+		double distanceRed = calcDistance(blue, green, red, 250, 190, 76);
+
+		if (distanceYello > DISTANCE_REJECT_CANDIDATE_WITH_COLOR && distanceGreen > DISTANCE_REJECT_CANDIDATE_WITH_COLOR && distanceRed > DISTANCE_REJECT_CANDIDATE_WITH_COLOR)
 		{
-			int blackNum = 0;
-			int areaWindow = 0;
-			int multipleNum = 3;
-			if (object[k].count < 20) multipleNum = 5;
-			int left = object[k].centerX - multipleNum * 3 * object[k].width;
-			int right = object[k].centerX + multipleNum * object[k].width / 2;
-			int up = object[k].centerY - multipleNum * object[k].height;
-			int down = object[k].centerY + multipleNum * object[k].height;
-
-			if (left < 0 || right>labelWidth || up < 0 || down >labelHeight) continue;
-
-			areaWindow = (right - left)*(down - up);
-
-			for (int i = left; i < right; i++)
-			{
-				for (int j = up; j < down; j++)
-				{
-					int blue = src.at<Vec3b>(j, i)[0];
-					int green = src.at<Vec3b>(j, i)[1];
-					int red = src.at<Vec3b>(j, i)[2];
-					if (calcDistance(blue, green, red, 20, 20, 20) < 40) blackNum++;
-				}
-			}
-
-			if (blackNum < areaWindow*0.5) object[k].isDeleted = true;
-			else continue;
-
-			blackNum = 0;
-			areaWindow = 0;
-			left = object[k].centerX - multipleNum * object[k].width / 2;
-			right = object[k].centerX + multipleNum * 3 * object[k].width;
-			up = object[k].centerY - multipleNum * object[k].height;
-			down = object[k].centerY + multipleNum * object[k].height;
-
-			if (left < 0 || right>labelWidth || up < 0 || down >labelHeight) continue;
-
-			areaWindow = (right - left)*(down - up);
-
-			for (int i = left; i < right; i++)
-			{
-				for (int j = up; j < down; j++)
-				{
-					int blue = src.at<Vec3b>(j, i)[0];
-					int green = src.at<Vec3b>(j, i)[1];
-					int red = src.at<Vec3b>(j, i)[2];
-					if (calcDistance(blue, green, red, 30, 30, 30) < 40) blackNum++;
-				}
-			}
-
-			if (blackNum < areaWindow*0.5) object[k].isDeleted = true;
+			object[k].isDeleted = true;
 		}
 	}
 }
 
 
+void checkArea2(Object object[], Mat& src, Mat& original, int label[])
+{
+	for (int i = labelWidth / 10 - 1; i < labelWidth / 10 * 9 + 1; i++)
+	{
+		for (int j = 0; j < labelHeight / 3 * 2 + 1; j++)
+		{
+			if (label[j*labelWidth + i] > 0)
+			{
+				object[label[j*labelWidth + i] - 1].count++;
+				object[label[j*labelWidth + i] - 1].centerX += i;
+				object[label[j*labelWidth + i] - 1].centerY += j;
+				if (object[label[j*labelWidth + i] - 1].checking)
+					continue;
+				else
+				{
+					object[label[j*labelWidth + i] - 1].width++;
+					object[label[j*labelWidth + i] - 1].checking = true;
+				}
+			}
+		}
+
+		for (int k = 0; k < blackObjectN; k++)
+			if (object[k].checking) object[k].checking = false;
+	}
+
+	for (int i = 0; i < labelHeight / 3 * 2; i++)
+	{
+		for (int j = labelWidth / 10 - 1; j < labelWidth / 10 * 9 + 1; j++)
+		{
+			if (label[i*labelWidth + j] > 0)
+			{
+				if (object[label[i*labelWidth + j] - 1].checking)
+					continue;
+				else
+				{
+					object[label[i*labelWidth + j] - 1].height++;
+					object[label[i*labelWidth + j] - 1].checking = true;
+				}
+			}
+		}
+
+		for (int k = 0; k < blackObjectN; k++)
+			if (object[k].checking) object[k].checking = false;
+	}
+
+	for (int k = 0; k < blackObjectN; k++)
+	{
+		object[k].centerX /= object[k].count;
+		object[k].centerY /= object[k].count;
+
+		if (object[k].count > 2)
+		{
+			int maxLenght = max(object[k].height, object[k].width);
+			int minLenght = min(object[k].height, object[k].width);
+			if (maxLenght < minLenght*2)
+			{
+				object[k].isDeleted = true;
+			}
+		}
+	}
+}
+
 void main(void)
 {
 	/* Setting */
-	Mat image = imread("test2.jpg");
+	Mat image = imread("test1.jpg");
 	Mat grayScaleImage(image.rows, image.cols, CV_8UC1);
 	Mat topHatImage(image.rows, image.cols, CV_8UC1);
 	Mat thresholdedImage(image.rows, image.cols, CV_8UC1);
+	Mat cannyImage(image.rows, image.cols, CV_8UC1);
+	Mat findBlackImage(image.rows, image.cols, CV_8UC1, Scalar(0));
+	Mat blackObjectImage(image.rows, image.cols, CV_8UC1, Scalar(0));
 	Mat temp(image.rows, image.cols, CV_8UC1, Scalar(0));
 
-	label = new int[image.cols*image.rows];
+
+	int *label = new int[image.cols*image.rows];
 	labelWidth = image.cols;
 	labelHeight = image.rows;
 	for (int i = 0; i < image.cols*image.rows; i++) label[i] = 0;
@@ -307,47 +306,104 @@ void main(void)
 	makeWhite(topHatImage, image, topHatImage);
 
 	/* Thresholding */
-	threshold(topHatImage, thresholdedImage, THRESHOLD_UNDER, THRESHOLD_UPPER, CV_THRESH_BINARY);
+	threshold(topHatImage, thresholdedImage, THRESHOLD_LIGHT, 256, CV_THRESH_BINARY);
 	
 	/* First Filter(Area) */
-	objectN = find_components(thresholdedImage);
+	objectN = find_components(thresholdedImage, label);
 	Object* object = new Object[objectN];
-	checkArea(object, thresholdedImage, image);
+	checkArea(object, thresholdedImage, image, label);
+
+	/* Second Filter(Color) */
+	removeDifferColorCandidate(image, object);
 
 
-	//지워진거 삭제해주는 임시코드
-	for (int k = 0; k < objectN; k++)
+
+
+
+	for (int i = 0; i < image.rows; i++)
 	{
-		for (int i = 0; i < image.cols; i++)
+		for (int j = 0; j < image.cols; j++)
 		{
-			for (int j = 0; j < image.rows; j++)
+			if (i>image.rows / 3 * 2)
 			{
-				if (!object[k].isDeleted)
+				findBlackImage.at<uchar>(i, j) = 0;
+				continue;
+			}
+
+			if (j<image.cols / 10 || j>image.cols / 10 * 9)
+			{
+				findBlackImage.at<uchar>(i, j) = 0;
+				continue;
+			}
+
+			int blue = image.at<Vec3b>(i,j)[0];
+			int green = image.at<Vec3b>(i, j)[1];
+			int red = image.at<Vec3b>(i, j)[2];
+
+			double distanceBlack = calcDistance(blue, green, red, 0, 0, 0);
+
+			if (distanceBlack < DISTANCE_REJECT_CANDIDATE_WITH_COLOR)
+			{
+				if (abs(blue - green) < 30 && abs(green - red) < 30 && abs(blue - red) < 30)
 				{
-					if (k + 1 == label[j*image.cols + i])
-					{
-						temp.at<uchar>(j, i) = 255;
-					}
+					findBlackImage.at<uchar>(i, j) = 255;
 				}
+				else
+				{
+					findBlackImage.at<uchar>(i, j) = 0;
+				}
+			}
+			else
+			{
+				findBlackImage.at<uchar>(i, j) = 0;
 			}
 		}
 	}
 
-	/* Second Filter(Region Growing */
-	//checkRegion(object, thresholdedImage, grayScaleImage);
 
-	/* Final Filter(Is Background Black?) */
-	//checkSurround(object, image);
+	//int* label2 = new int[image.cols*image.rows];
+	//for (int i = 0; i < image.cols*image.rows; i++) label2[i] = 0;
+
+	//blackObjectN = find_components(findBlackImage, label2);
+	//Object* blackObject = new Object[objectN];
+	//checkArea2(blackObject, thresholdedImage, image, label2);
+
+	//for (int k = 0; k < blackObjectN; k++)
+	//{
+	//	if (blackObject[k].count < 4)
+	//	{
+	//		blackObject[k].isDeleted = true;
+	//	}
+
+	//	if (blackObject[k].count >= labelWidth * labelHeight * 0.04)
+	//	{
+	//		blackObject[k].isDeleted = true;
+	//	}
+	//}
+
+	//for (int k = 0; k < blackObjectN; k++)
+	//{
+	//	for (int i = 0; i < image.rows; i++)
+	//	{
+	//		for (int j = 0; j < image.cols; j++)
+	//		{
+	//			if (!blackObject[k].isDeleted)
+	//			{
+	//				if (label2[i*image.cols + j] == k + 1)
+	//				{
+	//					blackObjectImage.at<uchar>(i, j) = 255;
+	//				}
+	//			}
+	//		}
+	//	}
+	//}
 
 
 	imshow("image", image);
-	imshow("grayScaleImage", grayScaleImage);
-	imshow("topHatImage", topHatImage);
-	imshow("thresholdedImage", thresholdedImage);
-	imshow("temp", temp);
+	imshow("findBlackImage", findBlackImage);
 	waitKey(0);
 
 	delete[] label;
-	//delete[] object;
+	delete[] object;
 }
 
